75e30b98280f7e9c52d8bdf6b48a2a5f601d9257
Skewber <jeremia.e.neuhold@gmail.com>
2024-10-03 13:44:34 +0200

    Added functions for stacking and generation on master bias

--- test.py
+++ test.py
@@ -2,4 +2,4 @@ from source.data_reduction import DataReduction
 
 data = DataReduction('testdata/data_comb', 'testdata/data_comb/reduced_data')
 
-print(data.imagetypes)
+print(data.check_master('bias', master='master_bias.fits'))
